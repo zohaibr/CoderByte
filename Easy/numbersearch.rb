9d3a086b1ca2dde88f127cf3518f36f0edd8d786
zohaibr <zohaibr@gmail.com>
2015-12-25 16:56:33 -0600

    updated numbersearch.rb

--- Easy/numbersearch.rb
+++ Easy/numbersearch.rb
@@ -4,19 +4,26 @@
 #example above. So "55Hello" and "5Hello 5" should return two different answers. Each string will contain at least 
 #one letter or symbol. 
 
-def numbersearch(str)
+def number_search(str)
+  num_hold = []
+  num_arr = []
+
   str = str.split("")
-  sum = 0
+
+  str << "end"
+
   str.each do |char|
-    if char == char.to_i
-      sum += char
+    if %w(0 1 2 3 4 5 6 7 8 9).include? char
+      num_hold << char
+    else
+      num_arr  << num_hold.join.to_i
+      num_hold = []
     end
   end
 
-  sum
-
+  num_arr.inject(0) {|sum, n| sum + n}
 end
 
 
-puts numbersearch("75Number9") #== 84
-puts numbersearch("10 2One Number*1*") #== 13
+puts number_search("75Number9") #== 84
+puts number_search("10 2One Number*1*") #== 13
