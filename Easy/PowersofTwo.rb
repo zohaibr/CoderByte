51ac9f2cd5f4a2bfbcbc2ef9dd8923f998cfc555
zohaibr <zohaibr@gmail.com>
2015-12-26 13:13:08 -0600

    PowersofTwo

---
+++ Easy/PowersofTwo.rb
@@ -0,0 +1,12 @@
+#Using the Ruby language, have the function PowersofTwo(num) take the num parameter being passed which will be 
+#an integer and return the string true if it's a power of two. If it's not return the string false. 
+#For example if the input is 16 then your program should return the string true but if the input is 22 then
+#the output should be the string false.
+
+
+def powers_of_two(num)
+ (Math.log2(num) % 1) == 0 ? true : false
+end
+
+puts powers_of_two(4) == true
+puts powers_of_two(124) == false
