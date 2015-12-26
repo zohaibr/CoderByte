ec7314f1844946a84d383a2f058705e0a9d235af
zohaibr <zohaibr@gmail.com>
2015-12-26 01:19:30 -0600

    ThirdGreatest

---
+++ Easy/ThirdGreatest.rb
@@ -0,0 +1,21 @@
+#Using the Ruby language, have the function ThirdGreatest(strArr) take the array of strings stored in strArr and 
+#return the third largest word within in. So for example: if strArr is ["hello", "world", "before", "all"] your 
+#output should be world because "before" is 6 letters long, and "hello" and "world" are both 5, but the output 
+#should be world because it appeared as the last 5 letter word in the array. 
+#If strArr was ["hello", "world", "after", "all"] the output should be after because the first 
+#three words are all 5 letters long, so return the last one. The array will have at least three 
+#strings and each string will only contain letters.
+
+def ThirdGreatest(strArr)
+# word_length = Hash.new(0)
+#  strArr.each do |word|
+ #   word_length[word] += word.length
+#  end
+
+ sorted_arr = strArr.sort_by {|word| word.length}
+ sorted_arr[-3]
+end
+
+
+puts ThirdGreatest(["coder","byte","code"]) #== "code"
+puts ThirdGreatest(["abc","defg","z","hijk"]) #== "abc"
